8bf0c316550b578d4a2bf48ab52cb18beac453c5
Yohanes Gutema <yohgut@gmail.com>
2022-03-10 11:54:39 +0300

    my 5th task commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,11 @@
 #include <isotream.h>
 /** akkuma jeene yaa tahu
- *
+ *main
+ * 
+ * return
  * /
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
-return(0);
+return (0);
 }
