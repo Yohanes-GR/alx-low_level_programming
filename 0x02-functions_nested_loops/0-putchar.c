e47f50c682867fa25732c33dbe14101d94cd8038
Yohanes Gutema <yohgut@gmail.com>
2022-03-15 21:41:44 +0300

    my zero task commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,13 +6,13 @@
  **/
 int main(void)
 {
-	char yohanes[] = "_putchar";
+	char ermias[] = "_putchar";
 
 	int c;
 
 	for (c = 0; c < 8; c++)
 	{
-		_putchar(yohanes[c]);
+		_putchar(ermias[c]);
 	}
 	_putchar('\n');
 	return (0);
