5d730d6f20bbbbccf562ad0d6d40ebd8de072f05
Yohanes-GR <yohgut@gmail.com>
2022-04-02 02:43:57 +0300

    script

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,19 +1,25 @@
 #include "main.h"
 
 /**
- * _isupper - checks whether a character is uppercase.
- *@c: the integer to be supplied by user.
- * Return: 0 or 1.
+ * _isupper - checks for uppercase character
+ *
+ * @c: parameter hard-coded in main
+ *
+ * Return: 0 or 1
  */
 
 int _isupper(int c)
 {
-if (c >= 65 && c <= 90)
-{
-return (1);
-}
-else
-{
-return (0);
-}
+	int i;
+
+	if (c >= 'A' && c <= 'Z')
+	{
+		i = 1;
+		return (i);
+	}
+	else
+	{
+		i = 0;
+		return (i);
+	}
 }
