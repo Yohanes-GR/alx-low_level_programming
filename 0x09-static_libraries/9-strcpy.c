190edb6067357b7048a0354b5e6682e7dfabc0b3
Yohanes-GR <yohgut@gmail.com>
2022-04-02 03:21:05 +0300

    script

--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -1,23 +1,25 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
- * _strcpy - copies the string pointed to by src,
- * including the terminating null byte, to the
- * buffer pointed to by dest.
- * @dest: destination.
- * @src: source.
- * Return: the pointer to dest.
+ * _strcpy - function that copies the string pointed to by src,
+ * including the terminating null byte (\0), to the buffer pointed to by dest
+ *
+ * @dest: parameter defined in main
+ * @src: parameter defined in main
+ *
+ * Return: the pointer to dest
  */
+
 char *_strcpy(char *dest, char *src)
 {
-int count = 0;
+	char *tmp = dest;
 
-while (count >= 0)
-{
-*(dest + count) = *(src + count);
-if (*(src + count) == '\0')
-break;
-count++;
-}
-return (dest);
-}
+	while (*src != '\0')
+	{
+		*dest = *src;
+		src++;
+		dest++;
+	}
+	*dest = '\0';
+	return (tmp);
