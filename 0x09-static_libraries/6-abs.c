77d4efb42e3f268a69616fd2f79d983139640ab9
Yohanes-GR <yohgut@gmail.com>
2022-04-02 03:18:18 +0300

    script

--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -1,16 +1,18 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * _abs - function that computes the absolute value of an integer
- * @c:  is the int that will use for the argument of the function
- * Return: 0
+ * _abs - prints the absolute value of an integer
+ *
+ * @n: is hard-coded in main function
+ *
+ * Return: n
  */
-int _abs(int c)
+
+int _abs(int n)
 {
-if (c > 0 || c == 0)
-{
-return (c);
-}
-else
-return (c * -1);
+	if (n < 0)
+	{
+		n = n * (-1);
+	}
+	return (n);
 }
