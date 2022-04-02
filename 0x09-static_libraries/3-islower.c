5a31da2a0f8f2143c4159cb74965e5e3a14b16a0
Yohanes-GR <yohgut@gmail.com>
2022-04-02 03:04:56 +0300

    script

--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -1,17 +1,24 @@
 #include "main.h"
+
 /**
- * _islower - checks if a character is lowercase
- * @c:  is the int that will use for the argument of the function
- * Return: Always 0.
+ * _islower - checks for lowercase character
+ *
+ * @c: parameter hard-coded in main function
+ *
+ * Return: 0 or 1
  */
+
 int _islower(int c)
 {
-if (c >= 'a' && c <= 'z')
-{
-return (1);
-}
-else
-{
-return (0);
-}
+	int i;
+
+	if (c >= 'A' && c <= 'Z')
+	{
+		i = 0;
+	}
+	else if (c >= 'a' && c <= 'z')
+	{
+		i = 1;
+	}
+	return (i);
 }
