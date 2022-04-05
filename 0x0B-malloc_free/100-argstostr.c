4fb1aadc61329569013b8ef7b0eb751d6bce9b92
Yohanes Gutema <yohgut@gmail.com>
2022-04-05 18:12:51 +0300

    script

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,50 +1,54 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * *argstostr - convert arguments on command line to strings
- * @ac: int type
- * @av: pointer to array
- * Return: arguments as strings
- */
-
+  * argstostr - convert the params passed to the program to string
+  * @ac: the argument count
+  * @av: the argument vector
+  *
+  * Return: ...
+  */
 char *argstostr(int ac, char **av)
 {
-	int size, count, count1, count2 = 0;
-	char *ptr;
+	int ch = 0, i = 0, j = 0, k = 0;
+	char *s;
 
 	if (ac == 0 || av == NULL)
-	{
 		return (NULL);
+
+	while (i < ac)
+	{
+		while (av[i][j])
+		{
+			ch++;
+			j++;
+		}
+
+		j = 0;
+		i++;
 	}
 
-	for (count = 0; count < ac; count++)
-	{
-		for (count1 = 0; av[count][count1] != '\0'; count1++)
-		{
-			size += 1;
-		}
-		size += 1;
-	}
-	size += 1;
+	s = malloc((sizeof(char) * ch) + ac + 1);
 
-	ptr = malloc(sizeof(char) * size);
-	if (ptr == NULL)
+	i = 0;
+	while (av[i])
 	{
-		free(ptr);
-		return (NULL);
-	}
-	for (count = 0; count < ac; count++)
-	{
-		for (count1 = 0; av[count][count1] != '\0'; count1++)
+		while (av[i][j])
 		{
-			ptr[count2] = av[count][count1];
-			count2++;
+			s[k] = av[i][j];
+			k++;
+			j++;
 		}
-		ptr[count2] = '\n';
-		count2++;
+
+		s[k] = '\n';
+
+		j = 0;
+		k++;
+		i++;
 	}
-	ptr[count2] = '\0';
-	return (ptr);
+
+	k++;
+	s[k] = '\0';
+	return (s);
 }
