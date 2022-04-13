06b47b7dd82af29ca3f65025ae619835c92e8ef7
Yohanes Gutema <yohgut@gmail.com>
2022-04-13 20:40:42 +0300

    function that perform operation

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,12 +1,11 @@
-#ifndef _3_calc_h_
-#define _3_calc_h_
-
+#ifndef CALC_H
+#define CALC_H
 /**
-* struct op - Struct op
-* @op: The operator
-* @f: The function associated
-*/
-
+ * struct op - Struct op
+ *
+ * @op: operator
+ * @f: function
+ */
 typedef struct op
 {
 	char *op;
@@ -14,15 +13,9 @@ typedef struct op
 } op_t;
 
 int op_add(int a, int b);
-
 int op_sub(int a, int b);
-
 int op_mul(int a, int b);
-
 int op_div(int a, int b);
-
 int op_mod(int a, int b);
-
 int (*get_op_func(char *s))(int, int);
-
-#endif /* _3_calc_h_ */
+#endif
