30b15c281712af1c8c7ab7c9d8d199befb302a6f
Yohanes Gutema <yohgut@gmail.com>
2022-06-09 23:09:55 +0300

    number of elements in a linked

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,34 +1,18 @@
 #include "lists.h"
 
 /**
- * add_dnodeint_end - Add node in the end of the list
- * @head: Memory address of the head of the list
- * @n: Integer data
- *
- * Return: Head of the list
+ * free_dlistint - free a dlistint_t list
+ * @head: pointer to the beginning of the linked list
+ * Return: void
  */
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+void free_dlistint(dlistint_t *head)
 {
-	dlistint_t *new = malloc(sizeof(dlistint_t));
-	dlistint_t *last = *head;
+	dlistint_t *tmp;
 
-	if (new == NULL)
-		return (NULL);
-
-	new->n = n;
-	new->next = NULL;
-	new->prev = NULL;
-
-	if (*head == NULL)
+	while (head != NULL)
 	{
-		*head = new;
-		return (new);
+		tmp = head->next;
+		free(head);
+		head = tmp;
 	}
-
-	while (last->next != NULL)
-		last = last->next;
-
-	last->next = new;
-	new->prev = last;
-	return (new);
 }
